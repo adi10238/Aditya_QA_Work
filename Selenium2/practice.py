8beda03e22849cfbba818384c05053830aacb1f4
Aditya <adityasharma10238@gmail.com>
2025-03-22 02:04:14 +0530

    test

--- Selenium2/practice.py
+++ Selenium2/practice.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 
 
-driver = webdriver.chrome()
+driver = webdriver.Chrome()
+#navigate to URL
 driver.get("https://www.google.com")
+#maximize the browser
+driver.maximize_window()
+#take a screenshot
+driver.get_screenshot_as_file("ss.png")
